f863ba240a6baac4a8f4f0eb1e68ff030056e0a5
SAM NIJIN <103887376+SamNijin@users.noreply.github.com>
2022-10-01 21:13:08 +0530

    Add files via upload

---
+++ resume.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="application/views/css/styles.css">
+
+        <!-- Tailwind -->
+        <script src="https://cdn.tailwindcss.com"></script>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume | Sam Nijin</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="side">
+            <div class="imageres">
+                <img src="application/views/images/pic.png" alt="My Pic" class="imgchange">
+            </div>
+            <div class="sidecontent">
+            <div class="contact">
+                <div class="first">
+                    <p class="one">
+                        📧 <a href="mailto:samnijin562@gmail.com" class="contact">samnijin562@gmail.com</a>
+                    </p>
+                    <p class="two">
+                        <a class="num" href="tel:+919500303551">
+                            📞 +91 95 00 30 35 51
+                        </a>
+                    </p>
+                </div>
+                <div class="second">
+                    <p class="three">
+                        <a class="contact" href="https://goo.gl/maps/SBDTRNyhsmzdqZMq8">
+                            📍 Tamil Nadu, India
+                        </a>
+                    </p>
+                    <p class="four">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="35" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                        </svg>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  <p class="five"> linkedin.com/in/sam </p>
+                    </p>
+                </div>
+            </div>
+                <div class="sidehead">
+                    <h3 class="head">
+                        SKILLS
+                    </h3>
+                </div>
+                <div class="sidetext">
+                    <p class="text">
+                        HYPER TEXT MARKUP LANGUAGE
+                    </p>
+                    <p class="text">
+                        CASCADING STYLE SHEETS
+                    </p>
+                    <p class="text">
+                        Java Script
+                    </p>
+                    <p class="text">
+                        Python
+                    </p>
+                    <p class="text">
+                        Java
+                    </p>
+                    <p class="text">
+                        C Programming
+                    </p>
+                    <p class="text">
+                        UI/UX Designing
+                    </p>
+                    <p class="text">
+                        Adobe After Effects
+                    </p>
+                </div>
+                <div class="sidehead">
+                    <h3 class="head">
+                        LANGUAGES
+                    </h3>
+                </div>
+                <div class="text3">
+                    <ul>
+                            <li class="maine">
+                                Tamil
+                            </li>
+                            <li class="subie">
+                                Native or Mother Tongue
+                            </li>
+                    </ul>
+                    <ul>
+                            <li class="maine">
+                                English
+                            </li>
+                            <li class="subie">
+                                Full Professional Fluency
+                            </li>
+                    </ul>
+                    <ul>
+                            <li class="maine">
+                                Malayalam
+                            </li>
+                            <li class="subie">
+                                Understandable
+                            </li>
+                    </ul>
+                </div>
+                <div class="sidehead">
+                    <h3 class="head">
+                        INTERESTS
+                    </h3>
+                </div>
+                <div class="text3">
+                    <ul>
+                        <li class="maine">
+                        🏡 Gardening
+                        </li>
+                    </ul>
+                    <ul>
+                        <li class="maine">
+                            🌎 Sustainability
+                        </li>
+                    </ul>
+                    <ul>
+                        <li class="maine">
+                            🍝 Italian Cuisine
+                        </li>
+                    </ul>
+                    <ul>
+                        <li class="maine">
+                            🚴 Cycling
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="maintext">
+                <h2>
+                    Sam Nijin S
+                </h2>
+            </div>
+            <div class="intro">
+                <p class="int">
+                &emsp;&emsp;&emsp;&emsp;An innovative thinker, initiative taker and multi dimensional professional with exceptional logical and analytical skills possesses 3+ experience as a computer science engineer.
+                    Experience in designing, developing, implementing and testing computer-based hardware and software.
+                    Expert in dealing with components that are found inside computer equipment, as well as the actual devices that contain the computers and the software that runs on those machines.
+                    Proficient in design networks that connect computer systems.
+                    Expert in working on integrating software programs with computer hardware.
+                </p>
+            </div>
+            
+                <h3 class="headwrap">
+                    EDUCATION
+                </h3>
+                <div class="edusub">
+                    <ul class="main">
+                        <li>
+                            Bethlahem Matric. Hr. Sec. School, Karungal
+                        </li>
+                    </ul>
+                    <ul class="sub">
+                        <li>
+                            SSLC &emsp;&emsp;&emsp;&emsp;77.7 % &emsp;&emsp;&emsp;&emsp;2018
+                        </li>
+                    </ul>
+                    <ul class="main">
+                        <li>
+                            Oxford Matric. Hr. Sec. School, Tholayavattam
+                        </li>
+                    </ul>
+                    <ul class="sub">
+                        <li>
+                            HSC &emsp;&emsp;&emsp;&emsp;64.5 % &emsp;&emsp;&emsp;&emsp;2020
+                        </li>
+                    </ul>
+                    <ul class="main">
+                        <li>
+                            Mar Ephraem College of Engineering and Technology, Elavuvilai
+                        </li>
+                    </ul>
+                    <ul class="sub">
+                        <li>
+                            B.E. Computer Science and Engineering II Year
+                        </li>
+                    </ul>
+                </div>
+                    <h3 class="headwrap">
+                        PERSONAL DETAILS
+                    </h3>
+                <div class="sidetext2">
+                    <table id="perso">
+                    <tbody>
+                        <tr>
+                            <td>
+                                Name
+                            </td>
+                            <td>
+                                SAM NIJIN S
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Father's Name
+                            </td>
+                            <td>
+                                SUNIL K C
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Date of Birth
+                            </td>
+                            <td>
+                                05-06-2002
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Address
+                            </td>
+                            <td>
+                            9-55/1, Nedungambi Vilai, Chadayankuzhi, Killiyoor Post, Kanyakumari District, Tamil Nadu, India – 629 171.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Contact
+                            </td>
+                            <td>
+                                9500303551
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                E-Mail
+                            </td>
+                            <td>
+                                <a class="convert" href="mailto:samnijin562@gmail.com">
+                                    samnijin562@gmail.com
+                                </a>
+                            </td>
+                        </tr>
+</tbody>
+                        </table>
+                </div>
+                <h3 class="headwrap">
+                        DECLARATION
+                    </h3>
+                    <p class="int">
+                    &emsp;&emsp;&emsp;&emsp;I declare that the above particulars are true to the best of my knowledge.
+                    </p>
+        </div>
+    </div>
+</body>
+</html>
